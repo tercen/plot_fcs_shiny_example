b7cd2d1c6544661e45ecac7c66604e1390c02af6
wptmdoorn <wptmdoorn@gmail.com>
2021-06-10 12:27:26 +0000

    tabs in UI

--- ui.R
+++ ui.R
@@ -15,49 +15,60 @@ ui <- shinyUI(
     
     titlePanel("Flow Cytometry Plot"),
     
-    sidebarPanel(
-      textInput("title", "Graph title label", ""),
-      textInput("xlab", "X-axis label", ""),
-      textInput("ylab", "Y-axis label", ""),
-      textInput("legend", "Legend title label", ""),
-      textInput("breaks_x", "Breaks for X-Axis",
-                "0, 1e1, 1e2, 1e3, 1e4"),
-      textInput("breaks_y", "Breaks for Y-Axis",
-                "0, 1e1, 1e2, 1e3, 1e4"),
-      sliderInput("pointSize", "Individual point size",
-                  0, 10, 1, step = 1),
-      sliderInput("plotWidth", "Plot width (px)", 200, 2000, 500),
-      sliderInput("plotHeight", "Plot height (px)", 200, 2000, 500),
-      sliderInput("widthBasisX", "Width Basis (X)", -2000, 0, -10),
-      sliderInput("widthBasisY", "Width Basis (Y)", -2000, 0, -10),
-      sliderInput("negX", "Negative range in asymptotic decades (X)",
-                  0, 50, 0, step = 0.05),
-      sliderInput("negY", "Negative range in asymptotic decades (Y)",
-                  0, 50, 0, step = 0.05),
-      sliderInput(
-        "posX",
-        "Positive range in asymptotic decades (X)",
-        0,
-        50,
-        4.5,
-        step = 0.05
+    sidebarPanel(tabsetPanel(
+      tabPanel(
+        "Title and Graph",
+        textInput("title", "Graph title label", ""),
+        textInput("xlab", "X-axis label", ""),
+        textInput("ylab", "Y-axis label", ""),
+        textInput("legend", "Legend title label", ""),
+        sliderInput("plotWidth", "Plot width (px)", 200, 2000, 500),
+        sliderInput("plotHeight", "Plot height (px)", 200, 2000, 500),
       ),
-      sliderInput(
-        "posY",
-        "Positive range in asymptotic decades (Y)",
-        0,
-        50,
-        4.5,
-        step = 0.05
+      tabPanel(
+        "Breaks & Points",
+        textInput("breaks_x", "Breaks for X-Axis",
+                  "0, 1e1, 1e2, 1e3, 1e4"),
+        textInput("breaks_y", "Breaks for Y-Axis",
+                  "0, 1e1, 1e2, 1e3, 1e4"),
+        selectInput("scale", "Type of scale", choices = c("biexponential", "log10")),
+        sliderInput("pointSize", "Individual point size",
+                    0, 10, 1, step = 1),
       ),
-      selectInput("scale", "Type of scale", choices = c("biexponential", "log10")),
-      checkboxInput("labs", "Apply labels", 0),
-      checkboxInput("wrap", "Wrap panel grid", 0),
-      checkboxInput("fixed", "Axes equal for panels", 0),
-      checkboxInput("space", "Equal FCS space", 0),
-    ),
+      tabPanel(
+        "Bi-Exp",
+        sliderInput("widthBasisX", "Width Basis (X)", -2000, 0, -10),
+        sliderInput("widthBasisY", "Width Basis (Y)", -2000, 0, -10),
+        sliderInput("negX", "Negative range in asymptotic decades (X)",
+                    0, 50, 0, step = 0.05),
+        sliderInput("negY", "Negative range in asymptotic decades (Y)",
+                    0, 50, 0, step = 0.05),
+        sliderInput(
+          "posX",
+          "Positive range in asymptotic decades (X)",
+          0,
+          50,
+          4.5,
+          step = 0.05
+        ),
+        sliderInput(
+          "posY",
+          "Positive range in asymptotic decades (Y)",
+          0,
+          50,
+          4.5,
+          step = 0.05
+        )
+      ),
+      tabPanel(
+        "Misc",
+        checkboxInput("labs", "Apply labels", 0),
+        checkboxInput("wrap", "Wrap panel grid", 0),
+        checkboxInput("fixed", "Axes equal for panels", 0),
+        checkboxInput("space", "Equal FCS space", 0),
+      )
+    ), width=5),
     
-    mainPanel(uiOutput("reacOut"), )
-    
+    mainPanel(uiOutput("reacOut"), width=7)
   )
 )
